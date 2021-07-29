8a89985709159e618d3ad923fd979c3a4af8a4ba
Anaïs <anais75.b@gmail.com>
2021-07-29 19:01:20 +0200

    Connexion à la base de données

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,16 @@
 const express = require('express');
+
+const mongoose = require('mongoose');
+
 const app = express();
+
+// Connexion à la base de données
+mongoose.connect('mongodb+srv://website:WQxCxfsDGIHGAluc@cluster0.fjllj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    {useNewUrlParser: true,
+    useUnifiedTopology: true }
+)
+.then(() => console.log('Connected to MongoDB !'))
+.catch(() => console.log('Connection to MongoDB failed !'));
 
 app.use((req, res, next) => {
     console.log("Requête reçue !")
